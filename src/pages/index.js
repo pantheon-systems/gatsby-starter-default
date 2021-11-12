f20aa737a9f0dd7253c9d79e09bda7b0649d47e1
Wei Jie Cheng <weijie.cheng@pantheon.io>
2021-11-12 15:23:56 -0800

    Change to period

--- src/pages/index.js
+++ src/pages/index.js
@@ -20,7 +20,7 @@ const IndexPage = () => (
       style={{ marginBottom: `1.45rem` }}
     />
     <p>
-      <Link to="/page-2/">Go to page 2</Link> QA <br />
+      <Link to="/page-2/">Go to page 2</Link>. <br />
       <Link to="/using-typescript/">Go to "Using TypeScript"</Link>
     </p>
   </Layout>
