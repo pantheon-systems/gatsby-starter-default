c85e8baa312a496a3421ec7e6a886db0218ffc48
channambika-rhm-io <85926644+channambika-rhm-io@users.noreply.github.com>
2021-11-15 14:55:46 +0530

    DB-1071 test

--- src/pages/page-2.js
+++ src/pages/page-2.js
@@ -7,7 +7,7 @@ import SEO from "../components/seo"
 const SecondPage = () => (
   <Layout>
     <SEO title="Page two" />
-    <h1>Hi from the second page</h1>
+    <h1>Hi-Heloo from the second page</h1>
     <p>Welcome to page 2</p>
     <Link to="/">Go back to the homepage</Link>
   </Layout>
